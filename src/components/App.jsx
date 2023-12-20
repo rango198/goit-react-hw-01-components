4a9c886dac384dcfdf4de36ab3cd1c781bae6465
rango198 <137562811+rango198@users.noreply.github.com>
2023-12-20 21:58:27 +0200

    onepush

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import {  Profile } from './Profile/Profile.jsx';
+import user from './Profile/user.json'
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
